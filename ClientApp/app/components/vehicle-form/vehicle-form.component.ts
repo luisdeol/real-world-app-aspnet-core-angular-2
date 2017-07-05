b0612063b280a477745d9ae28fcd2a4ae9d6127e
Luis Felipe <luisfelipekai@gmail.com>
2017-07-05 19:47:45 -0300

    Populating Drop-down Lists.

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,15 +1,21 @@
+import { MakeService } from './../../services/make.service';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vehicle-form',
   templateUrl: './vehicle-form.component.html',
-  styleUrls: ['./vehicle-form.component.css']
+  styleUrls: ['./vehicle-form.component.css'], 
+  providers: [MakeService]
 })
 export class VehicleFormComponent implements OnInit {
-
-  constructor() { }
+  makes;
+  constructor(private makeService: MakeService) { }
 
   ngOnInit() {
+    this.makeService.getMakes().subscribe(makes=>{
+    this.makes = makes;
+      console.log("MAKES", this.makes);});
   }
 
 }
